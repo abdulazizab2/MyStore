c3d6d7764100d8c7acf667ff1ac4cc2d7e8480ee
abdulaziz <aabuainain@nic.gov.sa>
2022-12-27 11:10:42 +0300

    refactoring code to make product-list-item child of product-list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
+import { ProductListItemComponent } from './product-list-item/product-list-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductListComponent,
-    ProductDetailsComponent
+    ProductDetailsComponent,
+    ProductListItemComponent
   ],
   imports: [
     BrowserModule,
