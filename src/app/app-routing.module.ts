b7f9ff18008b44dfbb961b27fa0d8e049e57a1f7
abdulaziz <aabuainain@nic.gov.sa>
2022-12-30 17:10:43 +0300

    navigation bar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ProductListComponent } from './product-list/product-list.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
 
 const routes: Routes = [
   { path: '', component: ProductListComponent },
   { path: 'products/:id', component: ProductDetailsComponent },
+  {path: 'shopping-cart', component: ShoppingCartComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,19 @@ import { AppComponent } from './app.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
 import { ProductListItemComponent } from './product-list-item/product-list-item.component';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
+import { AddProductComponent } from './add-product/add-product.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductListComponent,
     ProductDetailsComponent,
-    ProductListItemComponent
+    ProductListItemComponent,
+    ShoppingCartComponent,
+    AddProductComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
